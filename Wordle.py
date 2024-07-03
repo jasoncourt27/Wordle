66819bb537cc81636d3834b4f6e848f70be0c766
jasoncourt27 <2143321@swansea.ac.uk>
2024-07-03 20:19:39 +0100

    Update
    Improved code quality and added colours for better user interface.

--- Wordle.py
+++ Wordle.py
@@ -1,4 +1,10 @@
 import random
+import re
+
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+GREY = '\033[90m'
+RESET = '\033[0m'
 
 five_letter_words = [
     "apple", "bread", "crane", "dream", "eagle", "flame", "grape", "heart", "ideal", "joker",
@@ -6,51 +12,53 @@ five_letter_words = [
     "urban", "vivid", "whale"
 ]
 
-word = random.choice(five_letter_words)
-wordList = list(word)
-currentList = []
-found = False
-turns = 6
-currentSolutionList = ['_','_','_','_','_']
+def choose_word():
+    return random.choice(five_letter_words)
 
+def validate_guess(guess):
+    return len(guess) == 5 and re.match("^[a-zA-Z]{5}$", guess)
 
-while found == False:
-    if turns > 0:
+def get_feedback(guess, word):
+    feedback = ""
+    word_dict = {}
+    
+    for i, letter in enumerate(word):
+        word_dict[letter] = word_dict.get(letter, 0) + 1
+
+    for i, letter in enumerate(guess):
+        if letter == word[i]:
+            feedback += GREEN + letter + RESET
+            word_dict[letter] -= 1
+        elif letter in word and word_dict[letter] > 0:
+            feedback += YELLOW + letter + RESET
+            word_dict[letter] -= 1
+        else:
+            feedback += GREY + letter + RESET
+
+    return feedback
+
+def play_game():
+    word = choose_word()
+    turns = 6
+    print("Welcome to Wordle!\n")
+
+    while turns > 0:
+        print(f"Turns Left: {turns}")
+
+        guess = input("Enter your guess (5 letters): ").lower()
+        while not validate_guess(guess):
+            print("Invalid input. Make sure it's a 5-letter word.")
+            guess = input("Enter your guess (5 letters): ").lower()
+
+        feedback = get_feedback(guess, word)
+        print("Feedback: ", feedback)
         print()
-        print("Turns Left:",turns)
-        print(currentSolutionList)
-        
-        validInput = False
-        while validInput == False:
-            guess = input("Enter your guess (5 letters): ")
-            charCount = 0
-            for i in guess:
-                charCount += 1
-            if charCount == 5:
-                validInput = True
-            else:
-                print("Invalid input. Must be 5 letters.")
+        if guess == word:
+            print(f"Well done! You found the word: {word}")
+            return
 
-        guessList = list(guess)
         turns -= 1
-        for i in range(len(guessList)):
-            for j in range(len(wordList)):
-                if guessList[i] == wordList[j]:
-                    if i == j:
-                        currentSolutionList[i] = guessList[i]
-                    else:
-                        print()
-                        print("'",guessList[i],"'", "is somewhere else in the word")
-        
-        if currentSolutionList == wordList:
-            found = True
-            print()
-            print("Well done! You found the word:", word)
-                       
 
-    else:
-        print()
-        print("You are out of turns")
-        print("The correct word is",word)
-        found = True
+    print(f"You are out of turns. The correct word was: {word}")
 
+play_game()
